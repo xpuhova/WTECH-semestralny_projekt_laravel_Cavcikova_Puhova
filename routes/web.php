2a64a7427cf3484d3f15750d75c4ffb09ff8c3fd
xpuhova <xpuhova@stuba.sk>
2026-04-06 18:05:32 +0200

    added routes to fix failing github tests

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing_page.index');
 })->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/profile', function () {
+    return view('pages.settings.⚡profile');
+})->middleware('auth')->name('profile.edit');
+
+Route::get('/security', function () {
+    return view('pages.settings.⚡security');
+})->middleware('auth')->name('security.edit');
